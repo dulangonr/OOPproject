a073135721c9a560028014a264c18de3c9cc9d05
dulangonr <153965198+dulangonr@users.noreply.github.com>
2024-11-08 04:45:25 -0800

    Update Person.php

--- Person.php
+++ Person.php
@@ -1,25 +1,29 @@
 <?php
 
 class Person {
-    protected $name;
-    protected $address;
-    protected $age;
+    private string $name;
+    private string $address;
+    private int $age;
 
-    public function __construct($name, $address, $age) {
+    public function __construct(string $name, string $address, int $age) {
         $this->name = $name;
         $this->address = $address;
         $this->age = $age;
     }
 
-    public function getName() {
+    public function getName(): string {
         return $this->name;
     }
 
-    public function getAddress() {
+    public function getAddress(): string {
         return $this->address;
     }
 
-    public function getAge() {
+    public function getAge(): int {
         return $this->age;
+    }
+
+    public function __toString(): string {
+        return "Name: $this->name, Address: $this->address, Age: $this->age";
     }
 }
